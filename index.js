a19d7c19039dc6897e4ca27cab675036e56d6275
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-02-15 22:41:37 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,644 @@
+/*var height = prompt("What is your name?")
+
+ console.log(height)
+
+ var width = prompt("What is your surname?")
+
+ console.log(width)*/
+
+
+
+ /*let a=3
+ let P=4*a;
+ console.log(P);*/
+
+ /*let a=5;
+ let S=a*a;
+ console.log(S);*/
+
+/*let a=5;
+let b=3;
+let S=a*b;
+let P=2*(a+b);
+console.log(S,P);*/
+
+/*let d=4;
+let x=3.14;
+let L=x*d;
+console.log(L);*/
+
+/*let a=2;
+let V=a*a*a;
+let S=6*a*a;
+console.log(V,S);*/
+
+/*let a=3;
+let b=2;
+let c=7;
+let S=2*(a*b+a*c+b*c), V=a*b*c;
+console.log(S,V);*/
+
+
+/*let R=8;
+let x=3.14;
+let S=x*R*R, L=x*R;
+console.log(S,L);*/
+
+/*let a=3, b=5;
+let x=(a+b)/2;
+console.log(x);*/
+
+/*let a=4, b=9;
+let x=Math.sqrt(a*b);
+console.log(x);*/
+
+/*let a=3, b=3;
+let x=a*b, y=a+b, z=Math.sqrt(a*a+b*b);
+console.log(x,y,z);*/
+ 
+
+/*let a=5, b=4;
+let c=Math.sqrt(a*a+b*b), P=a+b+c;
+console.log(c,P);*/
+
+/*let R=6, r=4, x=3.14;
+let S1=x*R*R, S2=x*r*r, S=S1-S2;
+console.log(S1,S2,S);*/
+
+/*let L=12.56, x=3.14;
+let R=L/(2*x), S=x*R*R;
+console.log(R,S);*/
+
+/*let x1=3, x2=5, y1=4, y2=8;
+let a=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+console.log(a);*/
+
+/*let x1=2, y1=4, x2=5, y2=6, x3=1, y3=9;
+let a=Math.sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1)), b=Math.sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)); 
+let c=a+b;
+console.log(a,b,c);*/
+
+/*let x1=2, y1=4, x2=5, y2=6;
+let x3=((x1+x2)/2), y3=((y1+y2)/2), z=x3*y3;
+console.log(x3,y3,z);*/
+
+
+/*let m=25685;
+console.log(Math.floor(m/1000));*/
+
+/*let X=12365478;
+console.log(Math.floor(X/1024));*/
+
+/*let x=25;
+console.log(Math.floor(x/10));
+console.log(x%10);*/
+
+/*let x=35;
+console.log(Math.floor(x/10+(x%10)));*/
+
+/*let x=48;
+console.log((x%10)),console.log(Math.floor(x/10))*/
+
+/*let x=prompt("son")
+if(x>999) {
+    console.log(Math.floor(x/1000))
+    console.log(Math.floor(x%100))
+}*/
+
+/*let S=5200;
+console.log(Math.floor(S)/60);*/
+
+
+/*let S=52659;
+console.log(Math.floor(S)/3600);*/
+
+/*let S=64259;
+console.log(Math.floor(S/60)), console.log(Math.floor(S/3600));*/
+
+/*let K = prompt("son");
+let one = prompt("dushanba");
+let two = prompt("seshanba");
+let three = prompt("chorshanba");
+let four = prompt("payshanba");
+let five = prompt("juma");
+let six = prompt("shanba");
+let seven = prompt("yakshanba");
+if(K<365){
+    console.log(Math.floor(K/53));
+    console.log(six);
+}*/
+
+/*let K=prompt("Sonni kiriting!")
+if(K>0){
+    console.log("Siz kiritgan son musbat!")
+}
+else{
+    console.log("Siz kiritgan son musbat emas!")
+}*/
+
+/*let K=prompt("Sonni kiriting!")
+if(K>0){
+    console.log(K/2);
+}
+else{
+    console.log("Siz kiritgan son toq son")
+}*/
+
+/*let jamisumma=2500000;
+let x,y,z;
+x=9433.34*500;
+y=10354.03*120;
+z=x+y;
+if(z<=jamisumma){
+    console.log("Oq yo'l, Alisher")
+}
+else{
+    console.log("Alisher ozgina sabr qilish kerak ekan!")
+}*/
+
+/*let height=prompt("Sonni kiriting");
+if(height>0){
+    console.log((height)+1)
+}
+else{
+    console.log("Musbat son kiritng!")
+}*/
+
+/*let x=5;
+if (x>0){
+    console.log(++x);
+    console.log(x+2);
+}*/
+
+/*let x=2, y=5, z=12;
+if(x,y,z>0){
+    console.log(++x);
+    console.log(x+2), console.log(++y), console.log(y+2);
+    console.log(--z), console.log(z-2);
+}*/
+/*let A=prompt("Haqiqiy sonni kiriting");
+let B=prompt("Haqiqiy sonni kiriting");
+if(A!==0, B!==0){
+    console.log(++A);
+    console.log(B+2);
+}
+else{
+    console.log("Nol soni kiritdingiz dasturda xatolik bor!")
+}*/
+
+/*let A=prompt("Sonni kiriting");
+let B=prompt("Sonni kiriting");
+if(A!==B, A!==0, B!==0){
+    console.log("A>B");
+}*/
+
+/*let A=5, B=2, C=16;
+if(A,B,C>0){
+    console.log(++A);
+    console.log(++B);
+}*/
+
+/*let A=prompt("Sonni kiriting");
+let B=prompt("Sonni kiriting");
+let C=prompt("Sonni kiriting");
+if(A,B,C>0){
+    console.log(2*A);
+    console.log(2*B);
+    console.log(2*C);
+}*/
+
+
+/*let A=prompt("Sonni kiriting");
+let B=prompt("Sonni kiriting");
+let C=prompt("Sonni kiriting");
+if(A===B, C!==0){
+    console.log(C);
+}*/
+
+/*let A=prompt("Sonni kiriting");
+let B=prompt("Sonni kiriting");
+let C=prompt("Sonni kiriting");
+let D=prompt("Sonni kiriting");
+if(A===B===C, D>0){
+    console.log(D);
+}*/
+
+/*let x=prompt("Sonni kiriting")
+if(x>0, x<=0){
+    console.log(Math.sin(x)*2);
+}*/
+
+/*let x=prompt("Sonni kiriting");
+if(x<-2, x>2){
+    console.log(2*x);
+}
+else{
+    console.log(-3*x);
+}*/
+
+/*let x=prompt("Sonni kiriting")
+if(x<=0, 0<x<2, x>=2){
+    console.log(-x);
+    console.log(x*x);
+    console.log(4);
+}*/
+
+
+/*let a=10;
+for(let i=0.1; i<=1; i=i+0.1){
+    console.log(i, "kg konfet bor", i*a)
+}*/
+
+/*let a=16, b=2;
+for(let b=2; b<=16; b=b+1){
+    console.log(b)
+}*/
+
+/*let a=16, b=5;
+for(let a=16; a>5; a=a-1){
+    console.log(a)
+}*/
+
+/*let a=10, k;
+for(let i=10; i>0; i=i+1){
+    k=a*i;
+    console.log()
+}*/
+
+/*let a=10, b=1, k;
+for(let i=10; i>0; i++){
+    k=(a+b)*i/2;
+    console.log(k);
+}*/
+
+
+// let jsNumber = document.getElementById("js-number");
+// let jsCurrency = document.getElementById("js-currency");
+// let jsBtn = document.getElementById("js-btn");
+// let jsResult = document.getElementById("js-result");
+
+
+
+// let dollar = 10800;
+// let rubl = 150;
+// let lira = 1257;
+
+// jsBtn.addEventListener("click", function (event) {
+//     if(jsCurrency.value ==="usd"){
+//         jsResult.textContent = "$" + (Number(jsNumber.value)/dollar)
+        
+//     }
+
+//          else if(jsCurrency.value ==="rub"){
+//              jsResult.textContent = "rubl" + (Number(jsNumber.value)/rubl)
+        
+//          }
+//          else if (jsCurrency.value ==="lira"){
+//             jsResult.textContent = "lr" + (Number(jsNumber.value)/lira)
+        
+//          }
+//     console.log(jsResult.textContent)
+// })
+
+
+//  document.forms.form.addEventListener("input", function(event){
+//      let x = document.forms.form.som.value;
+//      let y;
+//      y=x/10800;
+//      z=x/140.61
+//      document.forms.form.usd.value=y.toFixed(2);
+//      document.forms.form.rub.value=z.toFixed(2);
+//  })
+ 
+
+
+// let x=parseInt(prompt("a="));
+// let y=parseInt(prompt("b="));
+// let sum=x+y;
+// function add(x,y) {
+//     console.log("Sum(a,b)="+sum);
+// }
+// add();
+
+
+
+// let n=parseInt(prompt('Natural sonni kiriting: '));
+// let arr=[];
+
+// for (let i=1; i<=n; i++){
+//     if (i%2 === 1 ){
+//         arr.push(i);
+//     }
+// }
+// console.log(arr);
+
+
+
+// let arr = ["olma", "nok", "48","82"]
+// arr.sort();
+// console.log(arr)
+
+
+// const arr = [4,5,7,8,9]
+// let name = arr.splice([1])
+// console.log(name)
+
+
+
+// function findElement(arr, n)
+// {
+	
+// 	var leftMax = Array(n).fill(0);
+// 	leftMax[0] = Number.MIN_VALUE;
+
+// 	for(i = 1; i < n; i++)
+// 		leftMax[i] = Math.max(leftMax[i - 1],
+// 								arr[i - 1]);
+// 	var rightMin = Number.MAX_VALUE;
+
+
+// 	for(i = n-1; i >= 0; i--)
+// 	{
+		
+// 		if (leftMax[i] < arr[i] &&
+// 			rightMin > arr[i])
+// 			return i;
+
+// 		rightMin = Math.min(rightMin, arr[i]);
+// 	}
+
+// 	return 0;
+// }
+
+
+// var arr = [ 5, 1, 4, 6, 8, 10, 7, 9 ];
+// var n = arr.length;
+
+// document.write("Index of the element is " +
+// 			findElement(arr, n));
+
+
+// const number = prompt("Enter a number: ");
+
+
+// if(number % 2 == 0) {
+//     console.log("The number is even.");
+// }
+
+// else {
+//     console.log("The number is odd.");
+// }
+
+
+// CPP Program to print an arithmetic
+// progression series
+
+
+	// function printAP(a, d, n)
+	// {
+	// 	let curr_term;
+	// 	curr_term=a;
+	// 	for (let i = 1; i <= n; i++)
+	// 	{ document.write(curr_term + " ");
+	// 		curr_term =curr_term + d;
+	
+	// 	}
+	// }
+	// let a = 2;
+	// let d = 2;
+	// let n = 8;
+
+	// printAP(a, d, n);
+
+
+//     function printGP(a, r, n)
+// {
+//     let curr_term;
+//     for (let i = 0; i < n; i++) {
+//         curr_term = a * Math.pow(r, i);
+//         document.write(curr_term + " ");
+//     }
+// }
+   
+//     let a = 2;
+//     let r = 3;
+//     let n = 5; 
+//     printGP(a, r, n);
+
+// function CountingEvenOdd(arr, arr_size)
+// {
+//     let even_count = 0;
+//     let odd_count = 0;
+ 
+//     // massivdagi barcha qiymatlarni o'qish uchun sikl
+//     for (let i = 0; i < arr_size; i++) {
+         
+//     //     raqam to'liq yoki yo'qligini tekshirish
+//     // 2 ga bo'linadi
+
+//         if (arr[i] & 1 == 1)
+//             odd_count++;
+//         else
+//             even_count++;
+//     }
+ 
+//     document.write("Juft elementlar soni = " + even_count
+//         + "<br>" + "Toq elementlar soni = " + odd_count);
+// }
+ 
+//     let arr = [ 2, 3, 4, 5, 6, 8, 12, 23, ];
+//     let n = arr.length;
+     
+
+//     CountingEvenOdd(arr, n);
+
+
+// function rearrange(arr,n)
+//     {
+    
+//         let i = -1, temp = 0;
+//         for (let j = 0; j < n; j++)
+//         {
+//             if (arr[j] < 0)
+//             {
+//                 i++;
+//                 temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+  
+//         let pos = i+1, neg = 0;
+//         while (pos < n && neg < pos && arr[neg] < 0)
+//         {
+//             temp = arr[neg];
+//             arr[neg] = arr[pos];
+//             arr[pos] = temp;
+//             pos++;
+//             neg += 2;
+//         }
+//     }
+
+// const arr = [2,4,6];
+// arr.push(arr.push(arr.pop()))
+// console.log(arr)  
+
+// function EvenOddProduct(arr, n)
+// {
+//     let even = 1;
+//     let odd = 1;
+ 
+//     for (let i = 0; i < n; i++) {
+
+//         if (i % 2 == 0)
+//             even *= arr[i];
+//         else
+//             odd *= arr[i];
+//     }
+ 
+//     document.write("Even Index Product : " + even + "<br>");
+//     document.write("Odd Index Product : " + odd);
+// }
+ 
+  
+//     let arr = [ 1, 2, 3, 4, 5, 6 ];
+//     let n = arr.length;
+ 
+//     EvenOddProduct(arr, n);
+
+// function fib(n){
+//     if (n <= 0) {
+//         return 0;
+//     }
+//     if(n <= 2) {
+//         return 1;
+//     }
+//     else {
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+// let arr = [];
+// for(let i = 0; i <= 10; i++){
+//     arr.push(fib(i))  
+// }
+// console.log(arr)
+
+	// function mostFrequent(arr, n)
+	// {
+		
+	// 	arr.sort();
+
+	// 	let max_count = 1, res = arr[0];
+	// 	let curr_count = 1;
+		
+	// 	for (let i = 1; i < n; i++)
+	// 	{
+	// 		if (arr[i] == arr[i - 1])
+	// 			curr_count++;
+	// 		else
+	// 		{
+	// 			if (curr_count > max_count)
+	// 			{
+	// 				max_count = curr_count;
+	// 				res = arr[i - 1];
+	// 			}
+	// 			curr_count = 1;
+	// 		}
+	// 	}
+	
+	// 	if (curr_count > max_count)
+	// 	{
+	// 		max_count = curr_count;
+	// 		res = arr[n - 1];
+	// 	}
+	// 	return res;
+	// }
+	
+
+
+	// 	let arr = [1, 5, 2, 1, 3, 2, 1];
+	// 	let n = arr.length;
+	// 	document.write(mostFrequent(arr,n));
+
+
+// function merge(x,y)
+// {
+// 	for(let i=0;i<y.length;i++)
+// 	{
+// 		if (x[x.length - 1] > y[i])
+// 			x.push(y[i]);
+// 	}
+// 	return x;
+// }
+
+// function mergeDel(l)
+// {
+// 	if (l.length == 1)
+// 		return l;
+
+// 	let m = Math.floor(l.length / 2);
+
+// 	let temp1 = l.slice(0, m);
+// 	let temp2 = l.slice(m, l.length);
+
+// 	return merge(mergeDel(temp1), mergeDel(temp2));
+// }
+// let arr=[5, 4, 3, 2, 1];
+// let ans = mergeDel(arr);
+// document.write("[ ");
+// document.write(ans.join(", "));
+// document.write("]<br>");
+
+// function indexOfIgnoreCase(s1, s2) {
+//     let s1Lower = s1.toLowerCase();
+//     let s2Lower = s2.toLowerCase();
+//     return s1Lower.indexOf(s2Lower);
+//   }  (s1, s2)=> sozlar kiritilsa masalan bit, it bolsa bit IT qaytaradi
+
+// function toFahrenheit(celsius){
+//     return 1.8 * celsius +32;
+//     }  50qaytaradi dastlab 50*1.8 shu tartibda davom etadi.
+
+// function hypotenuse(a,b){
+//     let cSquare = Math.pow(a,2) + Math.pow(b,2);
+//     return Math.sqrt(cSquare)
+//     }  c*c=a*a+b*b ishlaydi.
+
+// function add(s){
+//     let summand1 = parseInt(s,10);
+//     let indexPlus = s.indexOf('+');
+//     let sAfterPlus = s.substr(indexPlus + 1);
+//     let summand2 = parseInt(sAfterPlus, 10);
+//     return summand1 + summand2;
+// }  yig'indi hisoblab beradi,
+
+// function rotate(anArray){
+// 	let element0 = anArray.shift();
+// 	anArray.push(element0);
+// 	return anArray;
+// }
+
+// function concatUp(firstArray, secondArray){
+// 	if(secondArray.length>= firstArray.length){
+// 	return firstArray.concat(secondArray);
+// 	}
+	
+// 	return secondArray.concat(firstArray);
+// }                                             concatUp(['x'], ['a', 'b'])qaytaradi [ 'x', 'a', 'b' ]
+
+
+// function list(words) {
+
+// 	if (words.length === 0) {
+// 	  return '';
+// 	}
+  
+// 	if (words.length === 1) {
+// 	  return words[0];
+// 	}
+  
+// 	let wordsExLast = words.slice(0, words.length - 1);
+// 	let lastWord = words[words.length - 1];
+// 	return wordsExLast.join(', ') + ' and ' + lastWord;
+//   }             list(['C', 'C++', 'Python', 'PHP'])qaytaradi 'C, C++, Python and PHP'.
+
